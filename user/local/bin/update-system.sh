325a96ed6a46c59a89758f972b3c3959ebf3c582
Nathan Acks <nathan.acks@cardboard-iguana.com>
2021-08-10 20:04:18 -0600

    Rebuild initramfs on Kali

--- user/local/bin/update-system.sh
+++ user/local/bin/update-system.sh
@@ -9,3 +9,7 @@ if [[ -n "$(which flatpak)" ]]; then
 	flatpak update
 	flatpak uninstall --unused
 fi
+
+if [[ "$HOSTNAME" == "kali" ]] && [[ "$(uname -m)" == "aarch64" ]] && [[ -f /boot/initramfs.gz ]]; then
+	sudo mkinitramfs -o /boot/initramfs.gz $(ls -1 /lib/modules | grep -e '-Re4son-v8l+$' | sort | tail -1)
+fi
