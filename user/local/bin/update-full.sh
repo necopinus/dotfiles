f4b6cde8ecde14c304627866d5bdbc3c6927c20e
Nathan Acks <nathan.acks@cardboard-iguana.com>
2021-11-12 17:19:44 -0700

    rclone is no longer used

--- user/local/bin/update-full.sh
+++ user/local/bin/update-full.sh
@@ -5,8 +5,6 @@ UPDATE_PATH=$HOME/.local/bin
 [[ -x $UPDATE_PATH/update-system.sh          ]] && $UPDATE_PATH/update-system.sh
 
 [[ -x $UPDATE_PATH/update-zoom.sh            ]] && $UPDATE_PATH/update-zoom.sh
-[[ -x $UPDATE_PATH/update-rclone.sh          ]] && $UPDATE_PATH/update-rclone.sh
-[[ -x $UPDATE_PATH/update-rclonesync.sh      ]] && $UPDATE_PATH/update-rclonesync.sh
 [[ -x $UPDATE_PATH/update-gam.sh             ]] && $UPDATE_PATH/update-gam.sh
 [[ -x $UPDATE_PATH/update-youtube-dl.sh      ]] && $UPDATE_PATH/update-youtube-dl.sh
 [[ -x $UPDATE_PATH/update-yubikey-manager.sh ]] && $UPDATE_PATH/update-yubikey-manager.sh
