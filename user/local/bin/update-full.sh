e2f7048a7094a6d798877fa521e90ef4b4991aae
Nathan Acks <nathan.acks@cardboard-iguana.com>
2022-01-04 16:30:56 -0700

    Cleanup

--- user/local/bin/update-full.sh
+++ user/local/bin/update-full.sh
@@ -4,12 +4,10 @@ UPDATE_PATH=$HOME/.local/bin
 
 [[ -x $UPDATE_PATH/update-system.sh          ]] && $UPDATE_PATH/update-system.sh
 
-[[ -x $UPDATE_PATH/update-zoom.sh            ]] && $UPDATE_PATH/update-zoom.sh
 [[ -x $UPDATE_PATH/update-gam.sh             ]] && $UPDATE_PATH/update-gam.sh
 [[ -x $UPDATE_PATH/update-youtube-dl.sh      ]] && $UPDATE_PATH/update-youtube-dl.sh
 [[ -x $UPDATE_PATH/update-keybase.sh         ]] && $UPDATE_PATH/update-keybase.sh
 [[ -x $UPDATE_PATH/update-volatility.sh      ]] && $UPDATE_PATH/update-volatility.sh
 [[ -x $UPDATE_PATH/update-kerbrute.sh        ]] && $UPDATE_PATH/update-kerbrute.sh
-[[ -x $UPDATE_PATH/update-obsidian.sh        ]] && $UPDATE_PATH/update-obsidian.sh
 [[ -x $UPDATE_PATH/update-yubikey-manager.sh ]] && $UPDATE_PATH/update-yubikey-manager.sh
 [[ -x $UPDATE_PATH/update-deskreen.sh        ]] && $UPDATE_PATH/update-deskreen.sh
