eb49e615ba565f8f6796cf3095369095d3d5ac55
Nathan Acks <nathan.acks@cardboard-iguana.com>
2022-02-24 15:39:40 -0700

    Move to GAMADV-XTD3

--- user/local/bin/update-gam.sh
+++ user/local/bin/update-gam.sh
@@ -8,6 +8,12 @@ else
 	GLIBC_VERSION="2.31"
 fi
 
+if [[ "$(uname -m)" == "aarch64" ]]; then
+	ARCH="arm64"
+else
+	ARCH=x86_64
+fi
+
 # Get local version.
 #
 if [[ -f $HOME/.cache/versions/gam ]]; then
@@ -18,7 +24,7 @@ fi
 
 # Get remote version.
 #
-REMOTE_VERSION="$(curl -L -s https://api.github.com/repos/jay0lee/GAM/releases/latest | grep -Po '"tag_name": ?"v\K.*?(?=")')"
+REMOTE_VERSION="$(curl -L -s https://api.github.com/repos/taers232c/GAMADV-XTD3/releases/latest | grep -Po '"tag_name": ?"v\K.*?(?=")')"
 
 # Install/update if there is a new version.
 #
@@ -26,9 +32,9 @@ if [[ "$LOCAL_VERSION" != "$REMOTE_VERSION" ]]; then
 	BUILD_DIR="$(mktemp -d)"
 	(
 		cd "$BUILD_DIR"
-		curl -L -O https://github.com/jay0lee/GAM/releases/download/v${REMOTE_VERSION}/gam-${REMOTE_VERSION}-linux-x86_64-glibc${GLIBC_VERSION}.tar.xz
+		curl -L -O https://github.com/taers232c/GAMADV-XTD3/releases/download/v${REMOTE_VERSION}/gamadv-xtd3-${REMOTE_VERSION}-linux-${ARCH}-glibc${GLIBC_VERSION}.tar.xz
 		mkdir -p $HOME/.local/bin
-		tar -xJf gam-${REMOTE_VERSION}-linux-x86_64-glibc${GLIBC_VERSION}.tar.xz -C $HOME/.local/bin --strip-components=1 gam/gam
+		tar -xJf gamadv-xtd3-${REMOTE_VERSION}-linux-${ARCH}-glibc${GLIBC_VERSION}.tar.xz -C $HOME/.local/bin --strip-components=1 gamadv-xtd3/gam
 		mkdir -p $HOME/.cache/versions
 		echo "$REMOTE_VERSION" > $HOME/.cache/versions/gam
 	)
