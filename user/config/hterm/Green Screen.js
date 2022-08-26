0e97e57148a9b10cb77dc256aa9e2d9bb848a542
Nathan Acks <nathan.acks@cardboard-iguana.com>
2022-08-25 23:26:35 -0600

    Green Screen, take 2

--- user/config/hterm/Green Screen.js
+++ user/config/hterm/Green Screen.js
@@ -5,20 +5,20 @@
  *     https://github.com/chriskempson/base16-unclaimed-schemes/blob/master/greenscreen.yaml
  */
 black         = "#000000";
-red           = "#002200";
-green         = "#003300";
-yellow        = "#004400";
-blue          = "#005500";
-magenta       = "#006600";
-cyan          = "#007700";
-white         = "#00ee00";
-brightBlack   = "#001100";
-brightRed     = "#008800";
-brightGreen   = "#009900";
-brightYellow  = "#00aa00";
-brightBlue    = "#00bb00";
-brightMagenta = "#00cc00";
-brightCyan    = "#00dd00";
+red           = "#001100";
+green         = "#002200";
+yellow        = "#003300";
+blue          = "#004400";
+magenta       = "#005500";
+cyan          = "#006600";
+white         = "#007700";
+brightBlack   = "#008800";
+brightRed     = "#009900";
+brightGreen   = "#00aa00";
+brightYellow  = "#00bb00";
+brightBlue    = "#00cc00";
+brightMagenta = "#00dd00";
+brightCyan    = "#00ee00";
 brightWhite   = "#00ff00";
 
 t.prefs_.set("color-palette-overrides", [
@@ -29,7 +29,7 @@ t.prefs_.set("color-palette-overrides", [
 t.prefs_.set("foreground-color", white);
 t.prefs_.set("background-color", black);
 
-t.prefs_.set("cursor-color", "rgba(0, 238, 0, 0.4)"); // white @ 40%
+t.prefs_.set("cursor-color", "rgba(0, 255, 0, 0.4)"); // brightWhite @ 40%
 
 /*
  * Additional options. See:
