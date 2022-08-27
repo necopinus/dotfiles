71e66268edaa0b8f9abf2fc7b7402b26cb060462
Nathan Acks <nathan.acks@cardboard-iguana.com>
2022-08-27 00:02:37 -0600

    Try a restricted color space with "Green Screen"

--- user/config/hterm/Green Screen.js
+++ user/config/hterm/Green Screen.js
@@ -5,20 +5,24 @@
  *     https://github.com/chriskempson/base16-unclaimed-schemes/blob/master/greenscreen.yaml
  */
 black         = "#000000";
-red           = "#002200";
-green         = "#004400";
-yellow        = "#006600";
-blue          = "#008800";
-magenta       = "#00aa00";
-cyan          = "#00cc00";
-white         = "#00ee00";
-brightBlack   = "#001100";
-brightRed     = "#003300";
-brightGreen   = "#005500";
-brightYellow  = "#007700";
-brightBlue    = "#009900";
-brightMagenta = "#00bb00";
-brightCyan    = "#00dd00";
+
+red           = "#003000";
+green         = "#003e00";
+yellow        = "#004c00";
+blue          = "#005a00";
+magenta       = "#006800";
+cyan          = "#007600";
+
+white         = "#00dd00";
+brightBlack   = "#002200";
+
+brightRed     = "#008900";
+brightGreen   = "#009700";
+brightYellow  = "#00a500";
+brightBlue    = "#00b300";
+brightMagenta = "#00c100";
+brightCyan    = "#00cf00";
+
 brightWhite   = "#00ff00";
 
 t.prefs_.set("color-palette-overrides", [
@@ -29,7 +33,7 @@ t.prefs_.set("color-palette-overrides", [
 t.prefs_.set("foreground-color", white);
 t.prefs_.set("background-color", black);
 
-t.prefs_.set("cursor-color", "rgba(0, 238, 0, 0.4)"); // white @ 40%
+t.prefs_.set("cursor-color", "rgba(0, 238, 0, 0.4)"); // #00ee00 @ 40%
 
 /*
  * Additional options. See:
