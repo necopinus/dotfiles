726b340d4ff1523210778560fb07a7c44ea57610
Nathan Acks <nathan.acks@cardboard-iguana.com>
2025-10-27 21:15:40 -0600

    Make fish colors a little darker

--- config/fish/conf.d/theme.fish
+++ config/fish/conf.d/theme.fish
@@ -7,18 +7,17 @@
 # Gruvbox Light
 #
 set -g fish_color_autosuggestion a89984
-set -g fish_color_cancel red
-set -g fish_color_command cyan
+set -g fish_color_cancel --bold red
+set -g fish_color_command --bold cyan
 set -g fish_color_comment 928374
 set -g fish_color_end d65d0e
-set -g fish_color_error red
-set -g fish_color_escape magenta
-set -g fish_color_history_current
-set -g fish_color_keyword blue
+set -g fish_color_error --bold red
+set -g fish_color_escape --bold magenta
+set -g fish_color_keyword --bold blue
 set -g fish_color_normal normal
-set -g fish_color_operator green
+set -g fish_color_operator --bold green
 set -g fish_color_param normal
-set -g fish_color_quote yellow
+set -g fish_color_quote --bold yellow
 set -g fish_color_redirection normal
 set -g fish_color_search_match --background f9f5d7
 set -g fish_color_selection normal --background d5c4a1
@@ -32,7 +31,6 @@ set -g fish_color_selection normal --background d5c4a1
 #set -g fish_color_end c35e0a
 #set -g fish_color_error red
 #set -g fish_color_escape magenta
-#set -g fish_color_history_current
 #set -g fish_color_keyword blue
 #set -g fish_color_normal normal
 #set -g fish_color_operator green
