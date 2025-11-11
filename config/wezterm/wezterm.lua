4e14a481c0a5019b77d6c7ebade20cd61a315ddf
Nathan Acks <nathan.acks@cardboard-iguana.com>
2025-11-12 08:43:21 +0900

    Initial WezTerm config (minimal, unfinished)

---
+++ config/wezterm/wezterm.lua
@@ -0,0 +1,8 @@
+local wezterm = require 'wezterm'
+local config = {}
+
+config.font = wezterm.font('JetBrainsMono Nerd Font Mono')
+config.font_size = 10.0
+config.color_scheme = 'Gruvbox light, medium (base16)'
+
+return config
