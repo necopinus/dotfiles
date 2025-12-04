673b44aa7635aa8ea30e224c091cde189614e916
Nathan Acks <nathan.acks@cardboard-iguana.com>
2025-12-04 15:13:59 -0700

    Fixes ssh config match logic

--- programs/ssh.nix
+++ programs/ssh.nix
@@ -5,14 +5,20 @@
 
     includes = ["hosts/*"];
     enableDefaultConfig = false;
-    extraConfig = "Match host * exec \"${pkgs.gnupg}/bin/gpg-connect-agent UPDATESTARTUPTTY /bye\"";
-    matchBlocks."*" = {
-      host = "* !127.0.0.1 !localhost";
-      addKeysToAgent = "yes";
-      compression = true;
-      forwardAgent = false;
-      serverAliveInterval = 15;
-      serverAliveCountMax = 4;
+    matchBlocks = {
+      "set-gpg-tty" = {
+        match = ''
+          host * exec "${pkgs.gnupg}/bin/gpg-connect-agent UPDATESTARTUPTTY /bye"
+        '';
+      };
+      "*" = {
+        host = "* !127.0.0.1 !localhost";
+        addKeysToAgent = "yes";
+        compression = true;
+        forwardAgent = false;
+        serverAliveInterval = 15;
+        serverAliveCountMax = 4;
+      };
     };
   };
 }
