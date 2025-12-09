5c1e009f585185d4a533da17618ae0e64f756de7
Nathan Acks <nathan.acks@cardboard-iguana.com>
2025-12-09 15:39:03 -0700

    Work around issue where Obsidian won't remember previously opened vaults

--- programs/obsidian.nix
+++ programs/obsidian.nix
@@ -1,5 +1,12 @@
 {...}: {
-  programs.obsidian.enable = true;
+  # Manually install Obsidian rather than using programs.obsidian.enable
+  # = true in order to work around vaults not being remembered. See:
+  #
+  #   https://github.com/nix-community/home-manager/issues/7406
+  # 
+  home.packages = with pkgs; [
+    obsidian
+  ];
 
   # Obsidian won't work with the Android VM's virtual GPU
   #
