7d3cd1e469490f883a1a367b0559e6f64863c5f7
Nathan Acks <nathan.acks@cardboard-iguana.com>
2026-01-20 14:52:28 -0700

    Try to get some protections using .gooseignore

--- programs/goose.nix
+++ programs/goose.nix
@@ -5,11 +5,12 @@
 }: let
   llmAgents = llm-agents.packages.${pkgs.stdenv.hostPlatform.system}; # Set in flake.nix overlay
 in {
-  home.packages = with pkgs; [
+  home.packages = [
     llmAgents.goose-cli # Set in flake.nix overlay
   ];
 
   xdg.configFile = {
     "goose/config.yaml".source = ../artifacts/config/goose/config.yaml;
+    "goose/.gooseignore".source = ../artifacts/config/goose/gooseignore;
   };
 }
