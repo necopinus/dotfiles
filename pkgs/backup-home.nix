3f4388b08a87a711ce09596ed1750ca47b04d1a4
Nathan Acks <nathan.acks@cardboard-iguana.com>
2026-03-07 09:08:53 -0700

    Backup Helix grammar sources, just in case

--- pkgs/backup-home.nix
+++ pkgs/backup-home.nix
@@ -38,6 +38,8 @@ writeShellApplication {
 
     mkBackupList "$XDG_CONFIG_HOME/nix"
 
+    mkBackupList "$XDG_CONFIG_HOME/helix/runtime/grammars/sources"
+
     mkBackupList "$HOME/.ssh"
     mkBackupList "$HOME/.android"
     mkBackupList "$XDG_CONFIG_HOME/shodan/api_key"
