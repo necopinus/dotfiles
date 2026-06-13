9a0d048152b6da9f5d1f4ac4cd89c3a2f3ffe95b
Nathan Acks <nathan.acks@cardboard-iguana.com>
2026-06-13 15:52:34 -0600

    Set git's user.signingKey dynamically

--- bundles/git/default.nix
+++ bundles/git/default.nix
@@ -17,8 +17,52 @@
     };
     signing = {
       format = "ssh";
-      key = "${config.home.homeDirectory}/.ssh/id_ed25519";
       signByDefault = true;
     };
+  };
+
+  # Wrap git to enable dynamically setting user.signingKey
+  #
+  xdg.configFile."bash/rc.d/git.sh" = {
+    enable = config.programs.bash.enable;
+    text = ''
+      git () {
+        if [[ -n "$GIT_SIGNING_KEY" ]]; then
+          ${config.programs.git.package}/bin/git -c user.signingKey="$GIT_SIGNING_KEY" "$@"
+        elif [[ -f "${config.home.homeDirectory}/.ssh/id_ed25519" ]]; then
+          ${config.programs.git.package}/bin/git -c user.signingKey="${config.home.homeDirectory}/.ssh/id_ed25519" "$@"
+        else
+          ${config.programs.git.package}/bin/git "$@"
+        fi
+      }
+    '';
+  };
+  xdg.configFile."zsh/rc.d/git.zsh" = {
+    enable = config.programs.zsh.enable;
+    text = ''
+      git () {
+        if [[ -n "$GIT_SIGNING_KEY" ]]; then
+          ${config.programs.git.package}/bin/git -c user.signingKey="$GIT_SIGNING_KEY" "$@"
+        elif [[ -f "${config.home.homeDirectory}/.ssh/id_ed25519" ]]; then
+          ${config.programs.git.package}/bin/git -c user.signingKey="${config.home.homeDirectory}/.ssh/id_ed25519" "$@"
+        else
+          ${config.programs.git.package}/bin/git "$@"
+        fi
+      }
+    '';
+  };
+  xdg.configFile."fish/rc.d/git.fish" = {
+    enable = config.programs.fish.enable;
+    text = ''
+      function git
+        if test -n "$GIT_SIGNING_KEY"
+          ${config.programs.git.package}/bin/git -c user.signingKey="$GIT_SIGNING_KEY" $argv
+        else if test -f "${config.home.homeDirectory}/.ssh/id_ed25519"
+          ${config.programs.git.package}/bin/git -c user.signingKey="${config.home.homeDirectory}/.ssh/id_ed25519" $argv
+        else
+          ${config.programs.git.package}/bin/git $argv
+        end
+      end
+    '';
   };
 }
