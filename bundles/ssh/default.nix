d548572da09a52bc0128357059e5dd8f451b1ddf
Nathan Acks <nathan.acks@cardboard-iguana.com>
2026-06-21 20:38:07 -0600

    Fix SSH Agent connections becoming lost on Tmux reconnect

--- bundles/ssh/default.nix
+++ bundles/ssh/default.nix
@@ -1,4 +1,8 @@
-{...}: {
+{
+  pkgs,
+  config,
+  ...
+}: {
   programs.ssh = {
     enable = true;
 
@@ -11,5 +15,34 @@
       ServerAliveInterval = 15;
       ServerAliveCountMax = 4;
     };
+  };
+
+  # Fix broken SSH Agent sockets on reconnected Tmux sessions
+  #
+  home.packages = with pkgs;
+    lib.optionals (pkgs.stdenv.isLinux && (("${config.home.username}" == "exedev") || ("${config.home.username}" == "necopinus"))) [
+      ssh-agent-switcher
+    ];
+
+  xdg.configFile."bash/rc.d/bat.sh" = {
+    enable = config.programs.bash.enable && pkgs.stdenv.isLinux && (("${config.home.username}" == "exedev") || ("${config.home.username}" == "necopinus"));
+    text = ''
+      ssh-agent-switcher --daemon &> /dev/null || true
+      export SSH_AUTH_SOCK="/tmp/ssh-agent.$USER"
+    '';
+  };
+  xdg.configFile."zsh/rc.d/bat.zsh" = {
+    enable = config.programs.zsh.enable && pkgs.stdenv.isLinux && (("${config.home.username}" == "exedev") || ("${config.home.username}" == "necopinus"));
+    text = ''
+      ssh-agent-switcher --daemon &> /dev/null || true
+      export SSH_AUTH_SOCK="/tmp/ssh-agent.$USER"
+    '';
+  };
+  xdg.configFile."fish/rc.d/bat.fish" = {
+    enable = config.programs.fish.enable && pkgs.stdenv.isLinux && (("${config.home.username}" == "exedev") || ("${config.home.username}" == "necopinus"));
+    text = ''
+      ssh-agent-switcher --daemon &> /dev/null || true
+      set -gx SSH_AUTH_SOCK "/tmp/ssh-agent.$USER"
+    '';
   };
 }
