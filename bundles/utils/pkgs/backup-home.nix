2d8142cfd4039f9a2cbe198ab7b94550a4fa636b
Nathan Acks <nathan.acks@cardboard-iguana.com>
2026-09-10 21:37:10 -0600

    Automated system update: Thu Sep 10 21:37:10 MDT 2026

--- bundles/utils/pkgs/backup-home.nix
+++ bundles/utils/pkgs/backup-home.nix
@@ -29,9 +29,9 @@ writeShellApplication {
     }
 
     if [[ "$(hostname)" == "kitsune" ]]; then
-      systemctl --user stop hermes-relay.service
-      systemctl --user stop hermes-gateway.service
-      systemctl --user stop hermes-dashboard.service
+      sudo systemctl --user -M "''${USER}@" stop hermes-relay.service
+      sudo systemctl --user -M "''${USER}@" stop hermes-gateway.service
+      sudo systemctl --user -M "''${USER}@" stop hermes-dashboard.service
 
       hermes backup
       crontab -l > "$HOME/exedev.crontab"
@@ -119,9 +119,9 @@ writeShellApplication {
     rm -f "$BACKUP_LIST"
 
     if [[ "$(hostname)" == "kitsune" ]]; then
-      systemctl --user start hermes-dashboard.service
-      systemctl --user start hermes-gateway.service
-      systemctl --user start hermes-relay.service
+      sudo systemctl --user -M "''${USER}@" start hermes-dashboard.service
+      sudo systemctl --user -M "''${USER}@" start hermes-gateway.service
+      sudo systemctl --user -M "''${USER}@" start hermes-relay.service
     fi
   '';
 }
--- bundles/utils/pkgs/update-system.nix
+++ bundles/utils/pkgs/update-system.nix
@@ -78,9 +78,9 @@ writeShellApplication {
     # Update Hermes
     #
     if [[ -n "$(which hermes 2> /dev/null)" ]]; then
-      systemctl --user stop hermes-relay.service
-      systemctl --user stop hermes-gateway.service
-      systemctl --user stop hermes-dashboard.service
+      sudo systemctl --user -M "''${USER}@" stop hermes-relay.service
+      sudo systemctl --user -M "''${USER}@" stop hermes-gateway.service
+      sudo systemctl --user -M "''${USER}@" stop hermes-dashboard.service
 
       hermes update --yes
 
@@ -124,9 +124,9 @@ writeShellApplication {
 
       hermes plugins install Codename-11/hermes-relay/plugin --enable --force
 
-      systemctl --user start hermes-dashboard.service
-      systemctl --user start hermes-gateway.service
-      systemctl --user start hermes-relay.service
+      sudo systemctl --user -M "''${USER}@" start hermes-dashboard.service
+      sudo systemctl --user -M "''${USER}@" start hermes-gateway.service
+      sudo systemctl --user -M "''${USER}@" start hermes-relay.service
     fi
 
     # Git repositories
